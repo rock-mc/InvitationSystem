7f52a0c404ceb48c67d14f2aa4d5f7895be3f520
CodingMan <CodingMan@uptt.cc>
2021-02-20 16:47:35 +0800

    Load by paper server success

---
+++ src/com/rock_mc/invitation_system/Main.java
@@ -0,0 +1,10 @@
+package com.rock_mc.invitation_system;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Invitation system active");
+    }
+}
