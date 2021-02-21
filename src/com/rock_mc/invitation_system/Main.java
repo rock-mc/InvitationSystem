e2922c13d719c051d05d5601352db5f3240bde69
CodingMan <CodingMan@uptt.cc>
2021-02-21 22:53:36 +0800

    Adjust to register event Listener

--- src/com/rock_mc/invitation_system/Main.java
+++ src/com/rock_mc/invitation_system/Main.java
@@ -2,9 +2,24 @@ package com.rock_mc.invitation_system;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("Invitation system active");
+        Log.logger = getLogger();
+
+        Log.server("Enable", "Active");
+
+        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+
+        Log.server("Enable", "Complete");
     }
+//    @EventHandler
+//    public void onLogin(PlayerLoginEvent e) {
+//        Player player = e.getPlayer();
+//        String uid = player.getUniqueId().toString();
+//
+//
+//    }
 }
