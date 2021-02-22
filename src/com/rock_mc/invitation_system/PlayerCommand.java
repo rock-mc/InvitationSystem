5f0a0c0cc893ad76aa27ec14f7e2ff30714aaf3d
CodingMan <CodingMan@uptt.cc>
2021-02-22 22:06:16 +0800

    Add command handler

---
+++ src/com/rock_mc/invitation_system/PlayerCommand.java
@@ -0,0 +1,21 @@
+package com.rock_mc.invitation_system;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class PlayerCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+        String cmd = command.getName().toLowerCase();
+
+        Log.server("recv cmd", cmd);
+
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            Log.cur_p(player, "You input cmd", cmd);
+        }
+        return false;
+    }
+}
