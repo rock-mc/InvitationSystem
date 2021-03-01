ab8cc615cb447ecfefa293e103e59d26800f97f4
CodingMan <CodingMan@uptt.cc>
2021-03-01 22:51:55 +0800

    Fixed prisoner time count error

--- src/com/rock_mc/invitation_system/Prisoner.java
+++ src/com/rock_mc/invitation_system/Prisoner.java
@@ -2,17 +2,25 @@ package com.rock_mc.invitation_system;
 
 public class Prisoner {
     public String uid;
+    public long basicTime;
     public long expiryTime;
+
     public Prisoner(String uid, int day){
         this.uid = uid;
 
-        setExpiryTime(day);
+        basicTime = java.time.Instant.now().getEpochSecond();
+        setExpiryTime(day, 0, 0, 0);
     }
-    public void setExpiryTime(int day){
-        expiryTime = java.time.Instant.now().getEpochSecond();
-        expiryTime += day * Util.DAY;
+    public Prisoner(String uid, int day, int hour, int min, int sec){
+        this.uid = uid;
+
+        basicTime = java.time.Instant.now().getEpochSecond();
+        setExpiryTime(day, hour, min, sec);
+    }
+    public void setExpiryTime(int day, int hour, int min, int sec){
+        expiryTime = day * Util.DAY + hour * Util.HOUR + min * Util.MIN + sec * Util.SEC;
     }
     public boolean isExpiry(){
-        return java.time.Instant.now().getEpochSecond() >= expiryTime;
+        return java.time.Instant.now().getEpochSecond() >= basicTime + expiryTime;
     }
 }
