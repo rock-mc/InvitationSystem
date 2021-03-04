2f32d4caf2d04bbb6dd477e2567cd3ec96c9de74
CodingMan <CodingMan@uptt.cc>
2021-03-04 16:15:36 +0800

    Develop input code command (WIP)

--- src/com/rock_mc/invitation_system/PlayerCommand.java
+++ src/com/rock_mc/invitation_system/PlayerCommand.java
@@ -1,9 +1,12 @@
 package com.rock_mc.invitation_system;
 
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.io.IOException;
 
 public class PlayerCommand implements CommandExecutor {
     @Override
@@ -16,6 +19,23 @@ public class PlayerCommand implements CommandExecutor {
         if (sender instanceof Player) {
             Player player = (Player) sender;
             Log.player(player, "You input cmd", cmd);
+
+
+            try {
+
+                String invitCode = null;
+                Log.player(player, "輸入驗證碼", ChatColor.GREEN, invitCode);
+                if(InvitSys.addWhitelist(player, invitCode)){
+                    Log.playerLog(player, ChatColor.GREEN + "驗證通過!");
+                }
+                else{
+                    Log.playerLog(player, ChatColor.RED + "驗證失敗!");
+                }
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
         return false;
     }
