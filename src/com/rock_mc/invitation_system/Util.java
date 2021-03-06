6b2290941ece40fb007bc05ad2ae05822261ea22
CodingMan <CodingMan@uptt.cc>
2021-03-06 19:27:47 +0800

    Adjust invit code to 6 bit upper case letter

--- src/com/rock_mc/invitation_system/Util.java
+++ src/com/rock_mc/invitation_system/Util.java
@@ -3,6 +3,7 @@ package com.rock_mc.invitation_system;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Random;
 import java.util.UUID;
 
 public class Util {
@@ -12,13 +13,14 @@ public class Util {
     static int HOUR = 60 * MIN;
     static int DAY = 24 * HOUR;
 
-    static void mkdir(String foler_path){
+    static void mkdir(String foler_path) {
         File directory = new File(foler_path);
-        if (directory.exists()){
+        if (directory.exists()) {
             return;
         }
         directory.mkdirs();
     }
+
     static void writeFile(String file_path, String data) throws IOException {
         File file = new File(file_path);
         // creates the file
@@ -30,8 +32,21 @@ public class Util {
         writer.flush();
         writer.close();
     }
-    static String genUUID(){
-        String uuid = UUID. randomUUID().toString().replace("-", "");
-        return uuid;
+
+    static String genUUID() {
+//        String uuid = UUID. randomUUID().toString().replace("-", "");
+//        return uuid;
+
+        String result = "";
+
+        Random r = new Random();
+
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (int i = 0; i < InvitSys.INVIT_CODE_LENGTH; i++) {
+//            System.out.println(alphabet.charAt(r.nextInt(alphabet.length())));
+
+            result += alphabet.charAt(r.nextInt(alphabet.length()));
+        }
+        return result;
     }
 }
