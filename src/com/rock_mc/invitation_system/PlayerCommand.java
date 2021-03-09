fe37f661bdd83df2f6701e7eee5548c6e2a047fc
CodingMan <CodingMan@uptt.cc>
2021-03-09 14:00:40 +0800

    Adjust command to new version plugin interface

--- src/com/rock_mc/invitation_system/PlayerCommand.java
+++ src/com/rock_mc/invitation_system/PlayerCommand.java
@@ -42,7 +42,8 @@ public class PlayerCommand implements CommandExecutor {
             if (sender instanceof Player) {
                 player = (Player) sender;
 
-                PlayerInfo playerInfo = new PlayerInfo(player, 0);
+                // 取得玩家資料
+                PlayerInfo playerInfo = InvitSys.playerData.getPlayer(player.getUniqueId());
 
 //            try {
 
@@ -62,7 +63,7 @@ public class PlayerCommand implements CommandExecutor {
                     String invitCode = args[1];
 //                    Log.player(player, "輸入驗證碼", ChatColor.GREEN, invitCode);
 
-                    if (InvitSys.addWhitelist(player, invitCode)) {
+                    if (InvitSys.addWhitelist(player, player, invitCode)) {
                         Log.player(player, ChatColor.GREEN + "驗證通過!");
                         InvitSys.failList.remove(playerInfo.uuid);
                     } else {
@@ -75,30 +76,23 @@ public class PlayerCommand implements CommandExecutor {
                         Log.player(player, "您尚有 " + ChatColor.RED + (InvitSys.MAX_RETRY_TIME - failPlayer.failTime) + ChatColor.WHITE + " 次輸入機會");
 
                         if (failPlayer.failTime >= InvitSys.MAX_RETRY_TIME) {
-                            InvitSys.addBlacklist(player, InvitSys.MAX_RETRY_FAIL_BLOCK_DAY, 0, 0, 0);
+                            InvitSys.addBlacklist(player, player, InvitSys.MAX_RETRY_FAIL_BLOCK_DAY, 0, 0, 0);
                             InvitSys.failList.remove(playerInfo.uuid);
 
                             Event event = new InvitKickEvent(false, player, "抱歉，請勿亂猜驗證碼，冷靜個 " + InvitSys.MAX_RETRY_FAIL_BLOCK_DAY + " 天吧");
                             Bukkit.getPluginManager().callEvent(event);
+                        } else {
+                            InvitSys.failList.save();
                         }
                     }
                 } else if (args[0].equalsIgnoreCase("gencode")) {
-                    playerInfo = InvitSys.playerData.getPlayer(playerInfo.uuid);
 
                     if (playerInfo.invitationQuota <= 0 && !player.isOp()) {
                         Log.player(player, ChatColor.RED + "抱歉!你已經沒有邀請配額");
                         return true;
                     }
 
-                    if (player.isOp()) {
-                        Log.player(player, "OP 擁有無限邀請配額");
-                    }
-
-                    String invitCode = Util.genUUID().substring(0, 6).toUpperCase();
-
-                    while (InvitSys.playerData.isRepeatCode(invitCode)) {
-                        invitCode = Util.genUUID().substring(0, 6).toUpperCase();
-                    }
+                    String invitCode = InvitSys.genInvitCode();
 
                     Log.player(player, "邀請碼 ", ChatColor.GREEN, invitCode);
                     Log.player(player, "請妥善保存");
@@ -156,11 +150,7 @@ public class PlayerCommand implements CommandExecutor {
                             continue;
                         }
 
-                        if (playerInfo.invitationQuota >= 0) {
-                            playerInfo.invitationQuota = newQuota;
-                        } else {
-                            playerInfo.invitationQuota += newQuota;
-                        }
+                        playerInfo.invitationQuota += newQuota;
                     }
                 } else {
                     Log.player(player, "給予使用者邀請配額", ChatColor.GREEN, playerName);
@@ -176,11 +166,7 @@ public class PlayerCommand implements CommandExecutor {
                         return true;
                     }
 
-                    if (playerInfo.invitationQuota >= 0) {
-                        playerInfo.invitationQuota = newQuota;
-                    } else {
-                        playerInfo.invitationQuota += newQuota;
-                    }
+                    playerInfo.invitationQuota = newQuota;
                 }
                 InvitSys.playerData.save();
                 Log.player(player, "執行狀態", ChatColor.GREEN, "完成");
@@ -195,17 +181,21 @@ public class PlayerCommand implements CommandExecutor {
                 String playerName = args[1];
                 Log.player(player, "查詢使用者", ChatColor.GREEN, playerName);
 
-                Player unblockPlayer = Bukkit.getPlayer(playerName);
-                if (unblockPlayer == null) {
+                PlayerInfo playerInfo = InvitSys.playerData.getPlayer(playerName);
+                if (playerInfo == null) {
                     Log.player(player, "查無此玩家", playerName);
                     return true;
                 }
-                PlayerInfo playerInfo = new PlayerInfo(unblockPlayer);
+                Player BukkitPlayer = Bukkit.getPlayer(playerInfo.uuid);
+                if (BukkitPlayer == null) {
+                    Log.player(player, "伺服器查無此玩家", playerName);
+                    return true;
+                }
 
                 Log.player(player, "============================");
                 Log.player(player, "玩家名稱", playerInfo.name);
 
-                if (unblockPlayer.isOp()) {
+                if (BukkitPlayer.isOp()) {
                     Log.player(player, "驗證狀態", ChatColor.GOLD, "Operator");
                     Log.player(player, "邀請配額", "∞");
                 } else if (InvitSys.whitelist.contains(playerInfo.uuid)) {
@@ -306,13 +296,13 @@ public class PlayerCommand implements CommandExecutor {
                     Log.player(player, "查無此玩家", blockPlayerName);
                     return true;
                 }
-                OfflinePlayer blockPlayer = Bukkit.getOfflinePlayer(playerInfo.uuid);
+                Player blockPlayer = Bukkit.getPlayer(playerInfo.uuid);
                 if (blockPlayer == null) {
                     Log.player(player, "伺服器查無此玩家", blockPlayerName);
                     return true;
                 }
 
-                InvitSys.addBlacklist(blockPlayer, blockDay, blockHour, blockMin, blockSec);
+                InvitSys.addBlacklist(player, blockPlayer, blockDay, blockHour, blockMin, blockSec);
                 InvitSys.failList.remove(playerInfo.uuid);
 
                 if (blockPlayer.isOnline()) {
